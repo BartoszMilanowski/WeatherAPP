0ed2503cfed59c2e211dae59859a0b5bc8d5d57e
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2023-09-11 11:55:27 +0200

    remove trash

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header } from "./Header";
 
 
 export const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'; 
-export const API_KEY = '';
+export const API_KEY = 'f5c0c39a31e588068cff89c82d020f70';
 
 function App() {
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import { Login } from './Login';
 import reportWebVitals from './reportWebVitals';
 import './style.css';
-import { Register } from './Register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,8 +11,6 @@ root.render(
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<App />} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/register' element={<Register />} />
   </Routes>
   </BrowserRouter>
 );
